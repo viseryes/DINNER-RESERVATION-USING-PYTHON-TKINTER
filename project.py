cefff32ae27c1b8092732323f2ac89f1ca8125e3
xoxo7047 <63516191+xoxo7047@users.noreply.github.com>
2020-07-18 21:26:16 +0530

    Update project.py

--- project.py
+++ project.py
@@ -5,7 +5,7 @@ from tkinter import messagebox
 def addbtn():
 
 	if contact.get() =='' or name.get()=='' or restime.get()=='' or persons.get() =='' or table.get()=='':
-		messagebox.showerror("field required","enter all fields")
+		messagebox.showerror("INVALID ENTRY","ENTER ALL FIELD")
 	else:
 		listbox.insert(END,([name.get()],contact.get(),restime.get(),persons.get(),table.get()))
 		
@@ -23,62 +23,63 @@ def clrbtn():
 
 
 root = Tk()
-root.geometry('600x400')
+root.geometry('550x370')
 root.title('DINNER RESERVATIONS')
 root.resizable(False,False)
+root.configure(bg='#373F51')
 
 # customer name
-name_label = Label(root, text = 'NAME')
+name_label = Label(root, text = 'NAME',bg='#373F51',font=("Helvetica",10,'bold'),fg="#FFFFFF")
 name_label.grid(row = 0, column = 0, pady = 10,padx = 15)
 name = StringVar()
-name_entry = Entry(root, width=30, textvariable= name)
+name_entry = Entry(root, width=30, textvariable= name,bd=2)
 name_entry.grid(row=0, column =1)
 
 # customer contact
-contact_label = Label(root, text = 'CONTACT NO.')
+contact_label = Label(root, text = 'CONTACT NO.',bg='#373F51',font=("Helvetica",10,'bold'),fg="#FFFFFF")
 contact_label.grid(row = 1, column = 0, padx= 15, pady = 10)
 contact = StringVar()
-contact_entry = Entry(root,width=30, textvariable= 	contact)
+contact_entry = Entry(root,width=30, textvariable= 	contact,bd=2)
 contact_entry.grid(row=1, column =1)
 
 
 # reservation time
-restime_label = Label(root, text = 'RESERVATION TIME')
+restime_label = Label(root, text = 'RESERVATION TIME',bg='#373F51',font=("Helvetica",10,'bold'),fg="#FFFFFF")
 restime_label.grid(row = 2, column = 0, padx = 0, pady = 20)
 restime = StringVar()
-restime_entry = Entry(root,width=30, textvariable = restime)
+restime_entry = Entry(root,width=30, textvariable = restime,bd=2)
 restime_entry.grid(row=2, column =1)
 
 
 # number of persons
-persons_label = Label(root, text = 'PERSONS')
+persons_label = Label(root, text = 'PERSONS',bg='#373F51',font=("Helvetica",10,'bold'),fg="#FFFFFF")
 persons_label.grid(row = 0, column = 2, padx = 30)
 persons = StringVar()
-persons_entry = Entry(root,width=5, textvariable = persons)
+persons_entry = Entry(root,width=5, textvariable = persons,bd=2)
 persons_entry.grid(row=0, column =3)
 
 # table number
-tabno_label = Label(root, text = 'TABLE NO.')
+tabno_label = Label(root, text = 'TABLE NO.',bg='#373F51',font=("Helvetica",10,'bold'),fg="#FFFFFF")
 tabno_label.grid(row = 1, column = 2, padx = 30)
 table = StringVar()
-tabno_entry = Entry(root,width=5, textvariable = table)
+tabno_entry = Entry(root,width=5, textvariable = table,bd=2)
 tabno_entry.grid(row=1, column =3)
 
 #buttons
 # add button
-add_btn = Button(root, text='Add entry', command = addbtn)
+add_btn = Button(root, text='ADD ENTRY', command = addbtn,relief=GROOVE, bg='#9AFF1F',fg="#000000")
 add_btn.grid(row = 3, column =0, pady = 10)
 #delete button
-del_btn = Button(root, text='Delete entry', command = delbtn)
+del_btn = Button(root, text='DELETE ENTRY', command = delbtn,relief=GROOVE, bg='#9AFF1F',fg="#000000")
 del_btn.grid(row = 3, column =1, pady = 10)
 #clear input button
-clr_btn = Button(root, text='Clear input', command = clrbtn)
+clr_btn = Button(root, text='CLEAR ENTRY', command = clrbtn,relief=GROOVE, bg='#9AFF1F',fg="#000000")
 clr_btn.grid(row = 3, column =3, pady = 10)
 
 #listbox
 listbox = Listbox(root, width = 70, height = 10)
-listbox.grid(row = 4, columnspan = 5) 
-scroll = Scrollbar(root,orient = VERTICAL)
+listbox.grid(row = 4, columnspan = 4) 
+scroll = Scrollbar(root,orient = VERTICAL,bg='black')
 scroll.grid(row=4, column=3,sticky='ns')
 listbox.configure(yscrollcommand= scroll.set)
 scroll.configure(command= listbox.yview)
